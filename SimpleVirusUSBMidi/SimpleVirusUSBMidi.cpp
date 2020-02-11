08a30d4c758e421b574b1bbe395e6dbbd48b7bf7
Antonio Malara <antonio.malara@gmail.com>
2020-02-11 23:22:28 +0100

    Cosmetics of main file

--- SimpleVirusUSBMidi/SimpleVirusUSBMidi.cpp
+++ SimpleVirusUSBMidi/SimpleVirusUSBMidi.cpp
@@ -1,149 +1,149 @@
 #include "USBVendorMIDIDriver.h"
 
+// - //
 
-// ^^^^^^^^^^^^^^^^^^ THINGS YOU MUST CUSTOMIZE ^^^^^^^^^^^^^^^^^^^^^^
+extern "C" void *NewSimpleVirusUSBMidiDriver(CFAllocatorRef allocator, CFUUIDRef typeID);
 
-// Generated UUID: A1F2F3CB-5A1F-4066-9255-96557C3DD539
+// - //
 
-#define kFactoryUUID CFUUIDGetConstantUUIDWithBytes(NULL, 0xA1, 0xF2, 0xF3, 0xCB, 0x5A, 0x1F, 0x40, 0x66, 0x92, 0x55, 0x96, 0x55, 0x7C, 0x3D, 0xD5, 0x39)
+#define kMyVendorID         0x0582
+#define kMyProductID        0x0008
+#define kTheInterfaceToUse  2
+#define kMyNumPorts         1
 
-#define kTheInterfaceToUse	2		// The third one
+// Factory UUID: A1F2F3CB-5A1F-4066-9255-96557C3DD539
+#define kFactoryUUID CFUUIDGetConstantUUIDWithBytes(\
+    NULL, \
+    0xA1, \
+    0xF2, \
+    0xF3, \
+    0xCB, \
+    0x5A, \
+    0x1F, \
+    0x40, \
+    0x66, \
+    0x92, \
+    0x55, \
+    0x96, \
+    0x55, \
+    0x7C, \
+    0x3D, \
+    0xD5, \
+    0x39  \
+)
 
-#define kMyVendorID			0x0582	// Roland Corporation
-#define kMyProductID		0x0008
-#define kMyNumPorts			1		// One in-out pair.
+// - //
 
-// __________________________________________________________________________________________________
-
-class GenericUSBMidiDriver : public USBVendorMIDIDriver {
+class SimpleVirusUSBMidiDriver : public USBVendorMIDIDriver {
 public:
-    GenericUSBMidiDriver();
-    ~GenericUSBMidiDriver();
     
+    SimpleVirusUSBMidiDriver()
+        : USBVendorMIDIDriver(kFactoryUUID)
+    {
+    }
+
+    ~SimpleVirusUSBMidiDriver()
+    {
+    }
+
     // USBMIDIDriverBase overrides
-    virtual bool            MatchDevice(        USBDevice *        inUSBDevice);
     
-    virtual MIDIDeviceRef    CreateDevice(        USBDevice *        inUSBDevice,
-                                         USBInterface *    inUSBInterface);
+    virtual bool MatchDevice(USBDevice * inUSBDevice)
+    {
+        const IOUSBDeviceDescriptor * devDesc = inUSBDevice->GetDeviceDescriptor();
+        
+        return ((USBToHostWord(devDesc->idVendor)  == kMyVendorID) &&
+                (USBToHostWord(devDesc->idProduct) == kMyProductID));
+    }
     
-    virtual USBInterface *    CreateInterface(    USBMIDIDevice *    usbmDev);
-    
-    virtual void        StartInterface(            USBMIDIDevice *    usbmDev);
+    virtual MIDIDeviceRef CreateDevice(USBDevice *    inUSBDevice,
+                                       USBInterface * inUSBInterface)
+    {
+        MIDIDeviceRef dev;
+        MIDIEntityRef ent;
+        
+        CFStringRef boxName = CFSTR("Roland PC-300 USB Keyboard");
+        
+        MIDIDeviceCreate(Self(),
+                         boxName,
+                         CFSTR("Roland"),
+                         boxName,
+                         &dev);
+        
+        // make entity for each port, with 1 source, 1 destination
+        for (int port = 1; port <= kMyNumPorts; ++port) {
+            char portname[64];
+            
+            if (kMyNumPorts > 1)
+                sprintf(portname, "Port %d", port);
+            else
+                CFStringGetCString(boxName, portname, sizeof(portname), kCFStringEncodingMacRoman);
+            
+            CFStringRef str = CFStringCreateWithCString(NULL, portname, 0);
+            MIDIDeviceAddEntity(dev, str, false, 1, 1, &ent);
+            CFRelease(str);
+        }
+        
+        return dev;
+    }
+
+    virtual USBInterface * CreateInterface(USBMIDIDevice * device)
+    {
+        USBInterface *intf = device->mUSBDevice->FindInterface(kTheInterfaceToUse, 0);
+        return intf;
+    }
+
     // pipes are opened, do any extra initialization (send config msgs etc)
+    virtual void StartInterface(USBMIDIDevice * usbmDev)
+    {
+
+    }
     
-    virtual void        StopInterface(            USBMIDIDevice *    usbmDev);
     // pipes are about to be closed, do any preliminary cleanup
+    virtual void StopInterface(USBMIDIDevice * usbmDev)
+    {
+        
+    }
     
-    virtual void        HandleInput(            USBMIDIDevice *    usbmDev,
-                                    MIDITimeStamp    when,
-                                    Byte *            readBuf,
-                                    ByteCount         readBufSize);
-    // a USB message arrived, parse it into a MIDIPacketList and
-    // call MIDIReceived
-    
-    virtual ByteCount    PrepareOutput(            USBMIDIDevice *    usbmDev,
-                                      WriteQueue &    writeQueue,
-                                      Byte *            destBuf);
+    // a USB message arrived, parse it into a MIDIPacketList and call MIDIReceived
+    virtual void HandleInput(USBMIDIDevice * usbmDev,
+                             MIDITimeStamp   when,
+                             Byte *          readBuf,
+                             ByteCount       readBufSize)
+    {
+        USBMIDIHandleInput(usbmDev, when, readBuf, readBufSize);
+    }
+
     // dequeue from WriteQueue into a single USB message, return
+    virtual ByteCount PrepareOutput(USBMIDIDevice * usbmDev,
+                                     WriteQueue &   writeQueue,
+                                     Byte *         destBuf)
+    {
+        ByteCount n =
+            USBMIDIPrepareOutput(usbmDev, writeQueue, destBuf, usbmDev->mOutPipe.mMaxPacketSize);
+        
+        if (n < usbmDev->mOutPipe.mMaxPacketSize) {
+            memset(destBuf + n, 0, usbmDev->mOutPipe.mMaxPacketSize - n);
+        }
+        
+        return usbmDev->mOutPipe.mMaxPacketSize;
+    }
+    
 };
 
-// __________________________________________________________________________________________________
+// - //
 
-// Implementation of the factory function for this type.
-extern "C" void *NewSimpleVirusUSBMidiDriver(CFAllocatorRef allocator, CFUUIDRef typeID);
 extern "C" void *NewSimpleVirusUSBMidiDriver(CFAllocatorRef allocator, CFUUIDRef typeID)
 {
 	// If correct type is being requested, allocate an
 	// instance of TestType and return the IUnknown interface.
+    
 	if (CFEqual(typeID, kMIDIDriverTypeID)) {
-		GenericUSBMidiDriver *result = new GenericUSBMidiDriver;
+		SimpleVirusUSBMidiDriver *result = new SimpleVirusUSBMidiDriver;
 		return result->Self();
-	} else {
+	}
+    else {
 		// If the requested type is incorrect, return NULL.
 		return NULL;
 	}
-}
-
-// __________________________________________________________________________________________________
-
-GenericUSBMidiDriver::GenericUSBMidiDriver() :
-USBVendorMIDIDriver(kFactoryUUID)
-{
-}
-
-GenericUSBMidiDriver::~GenericUSBMidiDriver()
-{
-}
-
-// __________________________________________________________________________________________________
-
-bool		GenericUSBMidiDriver::MatchDevice(	USBDevice *		inUSBDevice)
-{
-	const IOUSBDeviceDescriptor * devDesc = inUSBDevice->GetDeviceDescriptor();
-	if (USBToHostWord(devDesc->idVendor) == kMyVendorID) {
-		UInt16 devProduct = USBToHostWord(devDesc->idProduct);
-		if (devProduct == kMyProductID)
-			return true;
-	}
-	return false;
-}
-
-MIDIDeviceRef	GenericUSBMidiDriver::CreateDevice(	USBDevice *		inUSBDevice,
-                                                 USBInterface *	inUSBInterface)
-{
-	MIDIDeviceRef dev;
-	MIDIEntityRef ent;
-	//UInt16 devProduct = USBToHostWord(inUSBDevice->GetDeviceDescriptor()->idProduct);
-	
-	CFStringRef boxName = CFSTR("Roland PC-300 USB Keyboard");
-	MIDIDeviceCreate(Self(),
-                     boxName,
-                     CFSTR("Roland"),	// manufacturer name
-                     boxName,
-                     &dev);
-	
-	// make entity for each port, with 1 source, 1 destination
-	for (int port = 1; port <= kMyNumPorts; ++port) {
-		char portname[64];
-		if (kMyNumPorts > 1)
-			sprintf(portname, "Port %d", port);
-		else
-			CFStringGetCString(boxName, portname, sizeof(portname), kCFStringEncodingMacRoman);
-        
-		CFStringRef str = CFStringCreateWithCString(NULL, portname, 0);
-		MIDIDeviceAddEntity(dev, str, false, 1, 1, &ent);
-		CFRelease(str);
-	}
-    
-	return dev;
-}
-
-USBInterface *	GenericUSBMidiDriver::CreateInterface(USBMIDIDevice *device)
-{
-	USBInterface *intf = device->mUSBDevice->FindInterface(kTheInterfaceToUse, 0);
-	return intf;
-}
-
-void		GenericUSBMidiDriver::StartInterface(USBMIDIDevice *usbmDev)
-{
-}
-
-void		GenericUSBMidiDriver::StopInterface(USBMIDIDevice *usbmDev)
-{
-}
-
-void		GenericUSBMidiDriver::HandleInput(USBMIDIDevice *usbmDev, MIDITimeStamp when, Byte *readBuf, ByteCount readBufSize)
-{
-	USBMIDIHandleInput(usbmDev, when, readBuf, readBufSize);
-}
-
-ByteCount	GenericUSBMidiDriver::PrepareOutput(USBMIDIDevice *usbmDev, WriteQueue &writeQueue,
-                                              Byte *destBuf)
-{
-	ByteCount n =
-    USBMIDIPrepareOutput(usbmDev, writeQueue, destBuf, usbmDev->mOutPipe.mMaxPacketSize);
-	if (n < usbmDev->mOutPipe.mMaxPacketSize) {
-		memset(destBuf + n, 0, usbmDev->mOutPipe.mMaxPacketSize - n);
-	}
-	return usbmDev->mOutPipe.mMaxPacketSize;
 }
